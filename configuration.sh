c905c3bbd1b2ab361e5be437b588315514c9d65a
F1ower <root@smallflower.xin>
2019-10-14 23:48:39 +0800

    add .sh file

---
+++ configuration.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Version: V1.0
+#Author: f1ower
+#Description: Proxy for build V8 on Ubuntu.
+#LICENSE: GNU General Public License v2.0
+
+# install ssr and polipo
+# Dependent with some libraries.
+# Modify some configuration files.
